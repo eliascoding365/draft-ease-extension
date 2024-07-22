62065c8b4f41412f889c76f4c936ddf7ef71239a
eliascoding365 <eliasmontan10@gmail.com>
2024-07-22 18:10:27 -0300

    Create form submission, save the notes in local storage using the chrome api

---
+++ src/components/SaveButton/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from '../ui/button'
+
+const SaveButton = () => {
+  return (
+    <div>
+        <Button variant={'secondary'} className='text-sm py-1 px-3 font-bold' >Save</Button>
+    </div>
+  )
+}
+
+export default SaveButton
